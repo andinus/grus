1e6075d11caa9565e82707fff06e64a7c648464f
Andinus <andinus@nand.sh>
2020-04-06 23:21:05 +0530

    Fix logical error in getdir_unix.go

--- storage/getdir_unix.go
+++ storage/getdir_unix.go
@@ -11,26 +11,24 @@ import (
 // XDG_DATA_HOME is set & if that is not set then assume it to be the
 // default value which is $HOME/.local/share according to XDG Base
 // Directory Specification.
-func GetDir() string {
+func GetDir() (grusCacheDir string) {
 	cacheDir := SysDir()
 
 	// Grus cache directory is cacheDir/grus.
-	grusCacheDir := fmt.Sprintf("%s/%s", cacheDir,
+	grusCacheDir = fmt.Sprintf("%s/%s", cacheDir,
 		"grus")
 
-	return grusCacheDir
+	return
 }
 
 // SysDir returns the system data directory, this is useful for unveil in
 // OpenBSD.
-func SysDir() string {
-	if len(cacheDir) == 0 {
-		cacheDir = os.Getenv("XDG_DATA_HOME")
-	}
+func SysDir() (cacheDir string) {
+	cacheDir = os.Getenv("XDG_DATA_HOME")
 	if len(cacheDir) == 0 {
 		cacheDir = fmt.Sprintf("%s/%s/%s", os.Getenv("HOME"),
 			".local", "share")
 	}
 
-	return cacheDir
+	return
 }
